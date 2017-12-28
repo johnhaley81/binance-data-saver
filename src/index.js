ce53a944ee55e6e59e37e8ebd565fbbe95d57861
John Haley <john@hashback.io>
2017-12-28 10:53:28 -0700

    Put files into Year/Month/Day/Hour folders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 // @flow
 
+import fs from 'fs';
+
 import { argv } from 'yargs';
 import { BinanceWS } from 'binance';
 import jsonfile from 'jsonfile';
@@ -8,11 +10,42 @@ import jsonfile from 'jsonfile';
 const symbol = argv.symbol || 'XRPBTC';
 const binanceWS = new BinanceWS();
 
-const appendDataToFile = fileName => data =>
-  jsonfile.writeFileSync(fileName, data, { flag: 'a' });
+const normalizeDateSection = dateSection =>
+  dateSection > 9 ? dateSection.toString() : `0${dateSection}`;
 
-binanceWS.onDepthUpdate(symbol, appendDataToFile('./data/depth.json'));
+const getHourBucketFolder = date =>
+  `${date.getFullYear()}${normalizeDateSection(
+    date.getMonth()
+  )}${normalizeDateSection(date.getDate())}${normalizeDateSection(
+    date.getHours()
+  )}`;
 
-binanceWS.onAggTrade(symbol, appendDataToFile('./data/aggTrade.json'));
+const getFolderPath = () => `./data/${getHourBucketFolder(new Date())}`;
 
-binanceWS.onKline(symbol, '1m', appendDataToFile('./data/kline1m.json'));
+const ensureDir = (folderPath: string) => {
+  try {
+    fs.mkdirSync(folderPath);
+  } catch (e) {
+    if (e.code !== 'EEXIST') {
+      throw e;
+    }
+  }
+};
+
+const appendDataToFile = fileName => (data: {}) => {
+  const folderPath = getFolderPath();
+
+  try {
+    ensureDir(folderPath);
+    jsonfile.writeFileSync(`${folderPath}/${fileName}`, data, { flag: 'a' });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+  }
+};
+
+binanceWS.onDepthUpdate(symbol, appendDataToFile('depth.json'));
+
+binanceWS.onAggTrade(symbol, appendDataToFile('aggTrade.json'));
+
+binanceWS.onKline(symbol, '1m', appendDataToFile('kline1m.json'));
